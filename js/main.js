2670e9d42d08df0a92a0fb299e3de64feea4a46b
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2020-01-13 13:20:12 +0200

    js file and txt file added

---
+++ js/main.js
@@ -0,0 +1,29 @@
+console.log('Hello world consoled from external js');
+
+var a = 1;
+var b = 3;
+var c = 10;
+console.log(a + b - c);
+
+console.warn(b);
+console.error(a);
+
+var e = 2;
+var f = 4;
+if (e > f) {
+    console.log('e is greater thatn f');
+}
+
+if (e < f) {
+    console.log( e + ' is less than ' + f ) ;
+} else {
+    console.log('e is not less than f')
+}
+
+if ( e === 2) {
+    console.log('e is exactly 2');
+}
+
+
+
+
