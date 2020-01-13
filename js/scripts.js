f2b502c843ef614896a927321cd670bf30bdcd4f
ElluLaitinen <elisabet.laitinen@edu.bc.fi>
2020-01-13 13:20:02 +0200

    added test javascript

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+console.log('This is it');
+
+var a = 1;
+var b = 2;
+console.warn(a+b);
+
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);
+
+var a = 1;
+var b = 2;
+
+if (a > b) {
+    console.log('a is greater than b');
+}
+
+var a = 1;
+var b = 2;
+
+if (a < b) { 
+    /* do the following things 
+    if a is lesser than b */ 
+    console.log('a is lesser than b');
+    console.log(a + ' is lesser than ' + b);
+} else { // do the following things otherwise
+    console.log('a is not greater than b');
+}
+
+var a = 2;
+var b = 2;
+
+if (a < b) {
+    console.log('a is lesser than b');
+} else ( a > b) {
+    console.log('a is greater than b');
+}
